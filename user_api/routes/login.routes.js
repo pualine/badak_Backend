53e4452d46ad748af3773f2fa2ba62436737ba8c
pualine <prisypauline@gmail.com>
2024-04-10 11:51:17 -0700

    updated login routes

--- user_api/routes/login.routes.js
+++ user_api/routes/login.routes.js
@@ -2,6 +2,8 @@ import express from "express"
 import {  checkLoginCredentials, deleteLogin, getAllLogins, getLoginById, updateLogin } from "../controllers/login.controllers.js";
 
 const router = express.Router();
+
+// Define routes
 router.post("/login", checkLoginCredentials);
 router.get("/", getAllLogins);
 router.get("/", getLoginById);
